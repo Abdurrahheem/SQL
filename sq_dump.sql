db2343a5151c22ef5d1da241ba42c76fc49678b0
abduragim <a.shtanchaev@neurodatalab.com>
2020-08-26 23:02:52 +0300

    the very first commint'

---
+++ sq_dump.sql
@@ -0,0 +1,20 @@
+PRAGMA foreign_keys=OFF;
+BEGIN TRANSACTION;
+CREATE TABLE regions(
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name VARCHAR(255) NOT NULL UNIQUE,
+  active BOOLEAN NOT NULL DEFAULT TRUE
+);
+INSERT INTO regions VALUES(1,'Adyghya',1);
+CREATE TABLE cities(
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name VARCHAR(255) NOT NULL UNIQUE,
+  regions_id INTEGER NOT NULL,
+  active BOOLEAN NOT NULL DEFAULT TRUE,
+  FOREIGN KEY(regions_id) REFERENCES regions(id)
+);
+INSERT INTO cities VALUES(1,'maykop',1,1);
+DELETE FROM sqlite_sequence;
+INSERT INTO sqlite_sequence VALUES('regions',1);
+INSERT INTO sqlite_sequence VALUES('cities',1);
+COMMIT;
